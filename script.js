3d2d4101306835423aada535234c2a7608703a77
ohDASEUL <comeseul312@gmail.com>
2024-09-04 16:27:14 +0900

    달력만들기 - 현재 월 표시

--- script.js
+++ script.js
@@ -337,3 +337,11 @@ function handleTitleClick(){
   e.preventDefault();
   redirectToAppropriatePageData();
 }
+
+
+const date = new Date();
+
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+
+document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`
