c541b243cbc3d085c47c3a16fc8f6fb103dc199e
ohDASEUL <comeseul312@gmail.com>
2024-10-01 17:15:50 +0900

    자바스크립트 생성자 함수 복습 프로젝트 [나만의 레시피북] - 레시피 추가 후 콘솔창에 잘 나오는지 확인

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function myRecipe(name, ingredients, instructions, difficulty) {
     this.instructions = instructions;
     this.difficulty = difficulty;
 }
+
+const recipeForm = document.getElementById('add-recipe-form');
+const recipeName = document.getElementById('recipe-name');
+const ingredients = document.getElementById('ingredients');
+const instructions = document.getElementById('instructions');
+const difficulty = document.getElementById('difficulty');
+
+recipeForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    const name = recipeName.value
+    const ingredientList = ingredients.value
+    const instruction  = instructions.value
+    const level = difficulty.value
+
+    const newRecipe = new myRecipe(name, ingredientList,  instruction, level)
+
+    console.log(newRecipe)
+})
